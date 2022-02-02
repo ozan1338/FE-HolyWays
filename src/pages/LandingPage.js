bcac1ad73256b2de33a10d33fb071883390685b1
ozan1338 <akhmadfauzan1338@gmail.com>
2022-02-02 16:06:29 +0700

    fix issues in landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -96,7 +96,7 @@ export default function LandingPage() {
             {loading ? (
               <Loading />
             ) : (
-              funds.length === 0 ? null :
+              funds?.length === 0 ? null :
               funds?.map((item, index) => {
                 return (
                   <CardList
